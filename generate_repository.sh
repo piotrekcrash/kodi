85c39c5d76b0d650a8fda11d68200d067253a31f
piotrekcrash <piotreklyczko@gmail.com>
2021-04-02 11:54:55 +0200

    Fri 02 Apr 2021 11:54:55 AM CEST

--- generate_repository.sh
+++ generate_repository.sh
@@ -1,7 +1,7 @@
 python3 create_repository.py --datadir=resources/ \
 	https://github.com/piotrekcrash/kodi-plugin.video.toyago.git \
 	https://github.com/piotrekcrash/kodi-repository.piotrekcrash.git \
-	https://github.com/CastagnaIT/repository.castagnait.git:repository.castagnait
+	https://github.com/CastagnaIT/repository.castagnait.git#matrix:repository.castagnait
 
 git add *
 git commit -m "$(date)"
