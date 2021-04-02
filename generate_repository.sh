22af0d65f71fd7b58ac4976c79d5af9b8d8afa8e
piotrekcrash <piotreklyczko@gmail.com>
2021-04-02 12:05:19 +0200

    Fri 02 Apr 2021 12:05:19 PM CEST

--- generate_repository.sh
+++ generate_repository.sh
@@ -1,5 +1,6 @@
 python3 create_repository.py --datadir=resources/ \
 	https://github.com/piotrekcrash/kodi-plugin.video.toyago.git \
+	https://github.com/piotrekcrash/kodi-script.hyperion.switch.git \
 	https://github.com/piotrekcrash/kodi-repository.piotrekcrash.git \
 	https://github.com/CastagnaIT/repository.castagnait.git#matrix:repository.castagnait \
 	repositories/repository.kodiwpigulce.pl-1.0.1.zip \
