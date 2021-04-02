3c6b9ee851d854082713da4b05b2ccb03c26c1d8
piotrekcrash <piotreklyczko@gmail.com>
2021-04-02 11:48:13 +0200

    Fri 02 Apr 2021 11:48:13 AM CEST

--- generate_repository.sh
+++ generate_repository.sh
@@ -1,6 +1,8 @@
 python3 create_repository.py --datadir=resources/ \
 	https://github.com/piotrekcrash/kodi-plugin.video.toyago.git \
-	https://github.com/piotrekcrash/kodi-repository.piotrekcrash.git
+	https://github.com/piotrekcrash/kodi-repository.piotrekcrash.git \
+	repositories/repository.fanfilm.zip
+
 git add *
-git commit -m "1"
+git commit -m "$(date)"
 git push
