6b547a58f46e3790cb21987d534dcb5d7d084888
piotrekcrash <piotreklyczko@gmail.com>
2021-04-02 11:44:56 +0200

    1

--- generate_repository.sh
+++ generate_repository.sh
@@ -1,2 +1,6 @@
 python3 create_repository.py --datadir=resources/ \
-	https://github.com/piotrekcrash/kodi-plugin.video.toyago.git
+	https://github.com/piotrekcrash/kodi-plugin.video.toyago.git \
+	https://github.com/piotrekcrash/kodi-repository.piotrekcrash.git
+git add *
+git commit -m "1"
+git push
